36fb7f7c1a073823be676bea853a11168a7d7ea9
Mekhala Joshi <mekhalajoshi@gmail.com>
2019-12-28 19:03:02 -0500

    Updates

---
+++ precache-manifest.6c113fc7e86404677f1fb67141019911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743d11b181ed555a89e00790fa57fc24",
+    "url": "/Music-Search/index.html"
+  },
+  {
+    "revision": "06cbb428d468e66db84d",
+    "url": "/Music-Search/static/css/main.825665ba.chunk.css"
+  },
+  {
+    "revision": "768e22249978c1367351",
+    "url": "/Music-Search/static/js/2.89fbe848.chunk.js"
+  },
+  {
+    "revision": "ae9248e914d0efa2b63ea4fd97862ee4",
+    "url": "/Music-Search/static/js/2.89fbe848.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06cbb428d468e66db84d",
+    "url": "/Music-Search/static/js/main.72104ef1.chunk.js"
+  },
+  {
+    "revision": "f3b8cbd033055ac52a0b",
+    "url": "/Music-Search/static/js/runtime-main.e2d94aa7.js"
+  }
+]);
